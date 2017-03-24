0ba3a93aa97b7ec74d2d86d866ccd1674e45e9d5
Romain DUBOC <dubocr@gmail.com>
2017-03-24 18:52:21 +0100

    Update payments write rules

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,45 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 })
 
+exports.authorize = functions.https.onRequest((request, response) => {
+  // Retrieve payment card
+  if(request.body.amount <= 0) {
+        response.status(404).send("Bad amount");
+  } else {
+	  db.ref("cards/"+request.body.cardId).once("value", function(snapCard) {
+		const card = snapCard.val();
+		/* Check token */
+		if(card == null) {
+			response.status(404).send("Unknown card");
+		} else if(request.body.token !== card.token) {
+			response.status(300).send("Bad card token");
+		} else if(!card.isValid) {
+			response.status(300).send("Card not valid");
+		} else {
+			db.ref("accounts/"+request.body.to).once("value", function(snapTo) {
+				const toAccount = snapTo.val();
+				if(toAccount == null) {
+					response.status(404).send("Recipient account not found");
+				} else if(toAccount.maxBalance != null && (toAccount.balance + request.body.amount) > toAccount.maxBalance) {
+					response.status(300).send("Recipient balance over quota");
+				} else {
+					db.ref("accounts/"+card.accountId).once("value", function(snapFrom) {
+						const fromAccount = snapFrom.val();
+						if(fromAccount == null) {
+							response.status(404).send("Card account not found");
+						} else if((fromAccount.balance - request.body.amount) < 0) {
+							response.status(300).send("Insufficient funds");
+						} else {
+							response.status(200).send("Auth OK");
+						}
+					});
+				}
+			});
+		}
+	  });
+  }
+})
+
 exports.createAccount = functions.database.ref('users/{pushId}')
     .onWrite(event => {
       // Grab the current value of what was written to the Realtime Database.
@@ -27,8 +66,8 @@ exports.createAccount = functions.database.ref('users/{pushId}')
           balanceUpdated: new Date().getTime(),
           speed: 0,
           upcoming: 0,
-          name: user.firstName + ' ' + user.lastName.toUpperCase(),
-          imgUrl: user.image,
+          name: user.identity.firstName + ' ' + user.identity.lastName.toUpperCase(),
+          imageUrl: user.identity.imageUrl,
         };
         let accountRef = db.ref("accounts").push();
         db.ref('users/'+event.params.pushId+'/accountId').set(accountRef.key);
@@ -65,33 +104,42 @@ exports.processPayment = functions.database.ref('payments/{paymentId}')
         return db.ref("payments/"+event.params.paymentId+"/executed").set(updated);
     });
 
-exports.processPaymentAuth = functions.database.ref('paymentAuths/{paymentAuthId}')
+exports.processAuthorization = functions.database.ref('authorizations/{authId}')
     .onWrite(event => {
       // Grab the current value of what was written to the Realtime Database.
-      const paymentAuth = event.data.val();
-      if(paymentAuth.executed != null)
+      const authorization = event.data.val();
+      if(authorization.status != null)
           return null;
     
-      console.log("Process new payment authorization ", paymentAuth);
-      const paymentAuthRef = db.ref("paymentAuths/"+event.params.paymentAuthId+"/status");
-      const created = new Date().getTime();
-      db.ref("cards/"+paymentAuth.cardId).then(card => {
+      console.log("Process new payment authorization ", authorization);
+      const authRef = db.ref("authorizations/"+event.params.authId+"/status");
+      const dateTime = new Date().getTime();
+      db.ref("cards/"+authorization.cardId).once("value", function(snapshot) {
+          const card = snapshot.val();
+          console.log("Card used ", card);
+          
           /* Compute card token validity */
-          if(paymentAuth.token == card.token) {
-              cosole.error("Payment authorization rejected ", paymentAuth);
-              return paymentAuthRef.update({code: "reject", message: "Bad token"});
+          if(authorization.token !== card.token) {
+              console.error("Payment authorization rejected ", authorization);
+              return authRef.set({code: "reject", message: "Bad token"});
           }
           
           /* Store transaction */
-          let transaction = {
-              created: created,
-              paymentAuthId: event.params.paymentAuthId,
-              amount: paymentAuth.amount,
+          let payment = {
+              created: dateTime,
+              authorizationId: event.params.authId,
+              amount: authorization.amount,
               from: card.accountId,
-              to: paymentAuth.to
+              to: authorization.to
             };
-            db.ref("transactions").push().set(transaction).then(o => {
-                return db.ref("paymentAuths/"+event.params.paymentAuthId+"/executed").set(created);
+            const paymentRef = db.ref("payments").push();
+            return paymentRef.set(payment, function(error) {
+              if (error) {
+                console.error("Data could not be saved " + error);
+              } else {
+                console.log("Payment auth executed");
+                authRef.set({code: "executed", message: "Payment " + paymentRef.key, executed: dateTime});
+              }
             });
       });
     });
